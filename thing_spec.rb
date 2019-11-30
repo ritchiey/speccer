5d18b24bb106f9040563dc3142eeb1657e903879
Ritchie Young <ritchiey@gmail.com>
2019-11-30 09:31:51 +0800

    Initial

---
+++ thing_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+describe Haiku do
+  describe '.detected?' do
+    subject { described_class.detected?(text) }
+
+    context 'when passed haiku' do
+      let(:text) do
+        <<~TEXT
+          Five syllables here
+          And now you have seven more
+          And now five. Happy?
+        TEXT
+      end
+      it { is_expected_to eq(true) }
+    end
+
+    context 'when too many lines' do
+      let(:text) do
+        <<~TEXT
+          Five syllables here
+          And now you have seven more
+          And now five. Happy?
+          Extra line
+        TEXT
+      end
+      it { is_expected_to eq(false) }
+    end
+
+    context 'when no text'
+    context 'when too few lines'
+    context 'when not five syllables on first line'
+    context 'when not seven syllables on second line'
+    context 'when not five syllables on third line'
+  end
+end
